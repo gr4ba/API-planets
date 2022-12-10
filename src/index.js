f7a8b8cf8229c5d7ae60e5a91271f7f29e4da45b
Juan Diego Chura Vallejos <juan.chura@tecsup.edu.pe>
2022-12-10 05:39:26 -0500

    cambio index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
